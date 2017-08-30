b773a8e66a68bec0db402eddddaa28bb9479e445
AkinaySau <akinaysau@gmail.com>
2017-08-30 13:10:59 +0300

    init plugin

---
+++ sau-cp.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Sau Cp
+Plugin URI: http://a-sau.ru
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: AkinaySau
+Author URI: http://a-sau.ru
+License: A "Slug" license name e.g. GPL2
+*/
