469fd486a64921690dc977d7a5de62a09b52afe4
AkinaySau <akinaysau@gmail.com>
2017-08-30 18:05:10 +0300

    work fix

---
+++ lib/class/Options.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created for sau-cp.
+ * User: AkinaySau akinaysau@gmail.ru
+ * Date: 30.08.2017
+ * Time: 18:02
+ */
+
+namespace Sau\WP\Plugin;
+
+
+class Options {
+
+function make($type,$name,$title=''){
+
+
+
+}
+protected static
+
+}
